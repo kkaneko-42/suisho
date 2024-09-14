acab96604762819e7c36319a4a27c84e3de35a8a
kkaneko-window_laptop <gorutiru@gmail.com>
2024-09-14 21:20:29 +0900

    create dev env

---
+++ framework/core/core.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "core/macros.h"
---
+++ framework/core/macros.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/**
+ * @brief
+ *  \~japanese エンジンの名前空間
+ *  \~english Namespace of this engine
+ */
+#define ENGINE_NAMESPACE suisho
