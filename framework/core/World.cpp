90024432f792080690356cf1a6806293fffe76ee
kkaneko-window_laptop <gorutiru@gmail.com>
2024-09-16 00:54:11 +0900

    some world features implemented

--- framework/core/World.cpp
+++ framework/core/World.cpp
@@ -1,3 +1,11 @@
 #include "core/World.h"
 
+using namespace suisho;
 
+Entity World::createEntity() {
+    return reg_.create();
+}
+
+void World::destroyEntity(Entity e) {
+    reg_.destroy(e);
+}
--- framework/core/World.h
+++ framework/core/World.h
@@ -1,10 +1,62 @@
 #ifndef SUISHO_CORE_WORLD_H_
 #define SUISHO_CORE_WORLD_H_
 
+#include <entt/entity/registry.hpp>
+
 namespace suisho {
 
-class World {
+using Entity = entt::entity;
 
+/**
+ * @brief Entities, components and systems stored here.
+ */
+class World {
+public:
+    /**
+     * @brief Create a new Entity and add to this world
+     *
+     * @return Added eneity
+     */
+    Entity createEntity();
+
+    /**
+     * @brief Destroy the given entity. All components attached to e are also destroyed.
+     * 
+     * @param e Valid entity to destroy
+     * @throw std::invalid_argument e dosen't exists
+     */
+    void destroyEntity(Entity e);
+
+    /**
+     * @brief Add constructed component and attached to e
+     * 
+     * @tparam T Component type
+     * @tparam Args Component construction param types
+     * @param e Valid eneity to attach
+     * @param args Constructor params of T
+     * @throw std::invalid_argument e dosen't exists
+     */
+    template <class T, class... Args>
+    T& addComponent(Entity e, Args&&... args) {
+        return reg_.emplace<T>(e, std::forward<Args>(args)...);
+    }
+
+    /**
+     * @brief Remove components from e
+     * 
+     * @tparam T Component type to remove
+     * @tparam S Other component types to remove
+     * @param e Valid entity
+     * @return Actually removed components count
+     * @throw std::invalid_argument e dosen't exists
+     */
+    template <class T, class... S>
+    std::size_t removeComponent(Entity e) {
+        return reg_.remove<T, S...>(e);
+    }
+
+private:
+    entt::registry reg_;
 };
 
 } // namespace suisho
