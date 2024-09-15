8e43d568f1f40aa5b34c60da025921f4e3b658b4
kkaneko-window_laptop <gorutiru@gmail.com>
2024-09-15 23:47:34 +0900

    create world

---
+++ framework/core/World.h
@@ -0,0 +1,12 @@
+#ifndef SUISHO_CORE_WORLD_H_
+#define SUISHO_CORE_WORLD_H_
+
+namespace suisho {
+
+class World {
+
+};
+
+} // namespace suisho
+
+#endif // SUISHO_CORE_WORLD_H_
--- framework/core/core.h
+++ framework/core/core.h
@@ -1,4 +1,6 @@
 #ifndef SUISHO_CORE_H_
 #define SUISHO_CORE_H_
 
+#include "core/World.h"
+
 #endif // SUISHO_CORE_H_
