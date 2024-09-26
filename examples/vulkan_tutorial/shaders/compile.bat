1978ed0ce46eed0ee39e31e968c04fd9d60ba227
kkaneko <45529381+kkaneko-42@users.noreply.github.com>
2024-09-26 21:48:42 +0900

    modify dir structure

---
+++ examples/vulkan_tutorial/shaders/compile.bat
@@ -0,0 +1,5 @@
+C:/VulkanSDK/1.3.236.0/Bin/glslc.exe shader.vert -o vert.spv
+C:/VulkanSDK/1.3.236.0/Bin/glslc.exe shader.frag -o frag.spv
+rem C:/VulkanSDK/1.3.236.0/Bin/glslangValidator.exe --target-env vulkan1.0 -x -e main -o vert.spv shader.vert
+rem C:/VulkanSDK/1.3.236.0/Bin/glslangValidator.exe --target-env vulkan1.0 -x -e main -o frag.spv shader.frag
+pause
