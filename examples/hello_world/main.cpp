94cc32b25aa2f4111cb6f59d03c035e1a230f735
kkaneko-42 <45529381+kkaneko-42@users.noreply.github.com>
2024-10-01 01:03:15 +0900

    fetch freetype

--- examples/hello_world/main.cpp
+++ examples/hello_world/main.cpp
@@ -1,7 +1,22 @@
 ﻿#include <iostream>
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 int main() {
     // TODO: Engine features demo
+    FT_Library ft_lib;
+    FT_Error err = FT_Init_FreeType(&ft_lib);
+    if (err != FT_Err_Ok) {
+        std::cerr << "Failed to init freetype" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    err = FT_Done_FreeType(ft_lib);
+    if (err != FT_Err_Ok) {
+        std::cerr << "Failed to init freetype" << std::endl;
+        return EXIT_FAILURE;
+    }
+
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+    return EXIT_SUCCESS;
 }
