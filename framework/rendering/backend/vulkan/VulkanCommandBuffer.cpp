a21a46570a2a502dfae1298ecaf8f0a01a6c1965
kkaneko <45529381+kkaneko-42@users.noreply.github.com>
2024-10-05 21:20:25 +0900

    create VulkanCommandBuffer, but not tested

---
+++ framework/rendering/backend/vulkan/VulkanCommandBuffer.cpp
@@ -0,0 +1,52 @@
+#include "rendering/backend/vulkan/VulkanCommandBuffer.h"
+
+using namespace suisho;
+using namespace suisho::backend;
+
+bool VulkanCommandBuffer::beginRecording() {
+    VkCommandBufferBeginInfo info{};
+    info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    
+    return (vkBeginCommandBuffer(cmd_, &info) == VK_SUCCESS);
+}
+
+bool VulkanCommandBuffer::endRecording() {
+    return (vkEndCommandBuffer(cmd_) == VK_SUCCESS);
+}
+
+void VulkanCommandBuffer::beginRenderPass(
+    VkRenderPass pass, VkFramebuffer framebuffer,
+    std::vector<VkClearValue> clear_info,
+    const VkRect2D& area // FIXME: With (area_min, area_max)
+) {
+    VkRenderPassBeginInfo info{};
+    info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    info.renderPass = pass;
+    info.framebuffer = framebuffer;
+    info.renderArea.offset = area.offset;
+    info.renderArea.extent = area.extent;
+    info.clearValueCount = static_cast<uint32_t>(clear_info.size());
+    info.pClearValues = clear_info.data();
+
+    vkCmdBeginRenderPass(cmd_, &info, VK_SUBPASS_CONTENTS_INLINE);
+}
+
+void VulkanCommandBuffer::endRenderPass() {
+    vkCmdEndRenderPass(cmd_);
+}
+
+void VulkanCommandBuffer::setViewport(const VkViewport& viewport) {
+    vkCmdSetViewport(cmd_, 0, 1, &viewport);
+}
+
+void VulkanCommandBuffer::setScissor(const VkRect2D& scissor) {
+    vkCmdSetScissor(cmd_, 0, 1, &scissor);
+}
+
+void VulkanCommandBuffer::bindGraphicsPipeline(VkPipeline pipeline) {
+    vkCmdBindPipeline(cmd_, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+}
+
+void VulkanCommandBuffer::draw(uint32_t vertex_count) {
+    vkCmdDraw(cmd_, vertex_count, 1, 0, 0);
+}
---
+++ framework/rendering/backend/vulkan/VulkanCommandBuffer.h
@@ -0,0 +1,33 @@
+#ifndef SUISHO_RENDERING_BACKEND_VULKAN_COMMAND_BUFFER_H_
+#define SUISHO_RENDERING_BACKEND_VULKAN_COMMAND_BUFFER_H_
+
+#include <vulkan/vulkan.h>
+#include <vector>
+
+namespace suisho::backend {
+
+class VulkanCommandBuffer {
+public:
+    bool beginRecording();
+    bool endRecording();
+
+    void beginRenderPass(
+        VkRenderPass pass, VkFramebuffer framebuffer,
+        std::vector<VkClearValue> clear_info,
+        const VkRect2D& area // FIXME: With (area_min, area_max)
+    );
+    void endRenderPass();
+
+    void setViewport(const VkViewport& viewport);
+    void setScissor(const VkRect2D& scissor);
+    void bindGraphicsPipeline(VkPipeline pipeline);
+
+    void draw(uint32_t vertex_count);
+
+private:
+    VkCommandBuffer cmd_;
+};
+
+} // namespace suisho::backend
+
+#endif // SUISHO_RENDERING_BACKEND_VULKAN_COMMAND_BUFFER_H_
