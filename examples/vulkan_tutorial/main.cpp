a892031cbade30af05644040dc21a9e81ecf9f50
kkaneko <45529381+kkaneko-42@users.noreply.github.com>
2024-10-17 02:14:33 +0900

    impl createWorld()

--- examples/vulkan_tutorial/main.cpp
+++ examples/vulkan_tutorial/main.cpp
@@ -3,8 +3,25 @@
 
 using namespace suisho;
 
-int main() {
+static World createWorld() {
     World world;
+
+    const float scale = 0.2f;
+    const int objects_count_x = 8;
+    const int objects_count_y = 8;
+    for (int y = -objects_count_y / 2; y < objects_count_y / 2; ++y) {
+        for (int x = -objects_count_x / 2; x < objects_count_x / 2; ++x) {
+            auto e = world.createEntity();
+            const Vec3 pos = (scale + 0.01f) * Vec3(x, y, 0.0f);
+            world.addComponent<Transform>(e, pos, scale * Vec3::kOne);
+        }
+    }
+
+    return world;
+}
+
+int main() {
+    World world = createWorld();
     SystemScheduler scheduler;
 
     RenderingSystem rendering;
