a18a524b7e916d5aeb194e35ef30f724cc0961c4
kkaneko <45529381+kkaneko-42@users.noreply.github.com>
2024-10-20 00:20:05 +0900

    remove glfw context creation

--- framework/rendering/backend/vulkan/VulkanDevice.cpp
+++ framework/rendering/backend/vulkan/VulkanDevice.cpp
@@ -96,7 +96,6 @@ void VulkanDevice::initWindow() {
     window = glfwCreateWindow(kWindowWidth, kWindowHeight, "Suisho Engine", nullptr, nullptr);
     glfwSetWindowUserPointer(window, this);
     glfwSetFramebufferSizeCallback(window, onGlfwWindowResized);
-    glfwMakeContextCurrent(window);
 }
 
 VulkanImage VulkanDevice::createImage(
