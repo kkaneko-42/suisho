67d7ddc3f5b95368cfd651207df610d9c928c3cc
kkaneko <45529381+kkaneko-42@users.noreply.github.com>
2024-10-23 23:28:33 +0900

    create InputAction

---
+++ framework/input/InputAction.h
@@ -0,0 +1,23 @@
+#ifndef SUISHO_INPUT_ACTION_H_
+#define SUISHO_INPUT_ACTION_H_
+
+namespace suisho {
+
+class InputAction {
+public:
+    template <class Device>
+    void bind(int scancode) {
+
+    }
+
+    template <class T>
+    void getValue() const {
+
+    }
+
+private:
+};
+
+}
+
+#endif // SUISHO_INPUT_ACTION_H_
