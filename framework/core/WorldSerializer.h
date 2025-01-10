cc6ed71ac7dc585ad2918773560deb9bc9a5f781
kkaneko <gorutiru@gmail.com>
2025-01-11 03:03:25 +0900

    add comment into WorldSerializer.h

--- framework/core/WorldSerializer.h
+++ framework/core/WorldSerializer.h
@@ -7,10 +7,26 @@
 
 namespace suisho {
 
+/**
+ * @brief Save/Load World status
+ * 
+ */
 class WorldSerializer {
 public:
+    /**
+     * @brief Construct a new World Serializer object
+     * 
+     * @param world Serialize/Deserialized World
+     */
     WorldSerializer(World& world);
 
+    /**
+     * @brief Register component type information. If it is already registered, overwrite
+     * 
+     * @tparam T Registered type
+     * @param name Component name
+     * @return If it is registered for the first time (that is, not overwritten), true
+     */
     template <class T>
     static bool registerComponent(const std::string& name) {
         const bool is_created = (getLoaders().count(name) == 0);
@@ -25,17 +41,30 @@ public:
         return is_created;
     }
 
+    /**
+     * @brief Save the state of the World specified in constructor to the file
+     * 
+     * @param dst_path Path of the destination file
+     */
     void serialize(const std::string& dst_path) const;
+
+    /**
+     * @brief Write the state read from the file into the World specified in the constructor
+     * 
+     * @param src_path Path of reading file
+     */
     void deserialize(const std::string& src_path);
 
 private:
     World& target_;
 
+    // @see Static initialization order fiasco
     static std::unordered_map<entt::id_type, std::function<void(cereal::JSONOutputArchive&, const void*)>>& getSavers() {
         static auto savers = new std::unordered_map<entt::id_type, std::function<void(cereal::JSONOutputArchive&, const void* data)>>();
         return *savers;
     }
 
+    // @see Static initialization order fiasco
     static std::unordered_map<std::string, std::function<void(cereal::JSONInputArchive&, World&, Entity)>>& getLoaders() {
         static auto loaders = new std::unordered_map<std::string, std::function<void(cereal::JSONInputArchive&, World&, Entity)>>();
         return *loaders;
