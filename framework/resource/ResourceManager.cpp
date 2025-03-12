ae21a83332ab029951ada29edb2720f10b00b55b
kkaneko <gorutiru@gmail.com>
2025-03-13 02:10:34 +0900

    ResourceManager::load changed

--- framework/resource/ResourceManager.cpp
+++ framework/resource/ResourceManager.cpp
@@ -6,6 +6,21 @@ const std::string ResourceManager::kLocalScheme = "res://";
 std::string ResourceManager::res_dir_ = "./resources/";
 std::vector<std::shared_ptr<IResourceLoader>> ResourceManager::loaders_;
 
+std::shared_ptr<Resource> ResourceManager::load(const std::string& uri) {
+    auto loader_itr = std::find_if(
+        loaders_.begin(), loaders_.end(),
+        [&uri](const std::shared_ptr<IResourceLoader>& loader) {
+            return loader->isSupported(uri);
+        }
+    );
+
+    if (loader_itr == loaders_.end()) {
+        throw std::runtime_error("ResourceManager::load(): " + uri + " loader not found");
+    }
+
+    return std::shared_ptr<Resource>((*loader_itr)->load(uri), [loader = *loader_itr](Resource* res) { loader->unload(res); });
+}
+
 bool ResourceManager::addLoader(std::shared_ptr<IResourceLoader> loader) {
     if (std::find(loaders_.begin(), loaders_.end(), loader) != loaders_.end()) {
         return false;
--- framework/resource/ResourceManager.hpp
+++ framework/resource/ResourceManager.hpp
@@ -13,25 +13,7 @@ class ResourceManager {
 public:
     static const std::string kLocalScheme;
 
-    template <class T>
-    static std::shared_ptr<T> load(const std::string& uri) {
-        static_assert(std::is_base_of_v<Resource, T>, "T must be derived from Resource");
-
-        auto loader = std::find_if(
-            loaders_.begin(), loaders_.end(),
-            [&uri](const std::shared_ptr<IResourceLoader>& loader) {
-                return loader->isSupported(uri);
-            }
-        );
-
-        if (loader == loaders_.end()) {
-            throw std::runtime_error("ResourceManager::load(): " + uri + " loader not found");
-        }
-
-        auto loaded = std::shared_ptr<Resource>(loader->load(uri), [loader](Resource* res) { loader->unload(res); });
-        return std::static_pointer_cast<T>(loaded);
-    }
-
+    static std::shared_ptr<Resource> load(const std::string& uri);
     static bool addLoader(std::shared_ptr<IResourceLoader> loader);
     static void setResourceDirectory(const std::string& path);
 
