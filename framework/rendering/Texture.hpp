ab1ebc26dc22353f2c2a8f1c8648fe639abdc6f1
kkaneko <gorutiru@gmail.com>
2025-03-16 01:09:21 +0900

    create Texture

---
+++ framework/rendering/Texture.hpp
@@ -0,0 +1,22 @@
+#ifndef SUISHO_RENDERING_TEXTURE_H_
+#define SUISHO_RENDERING_TEXTURE_H_
+
+#include "resource/Resource.hpp"
+#include "rendering/backend/vulkan/VulkanTexture.hpp"
+
+namespace suisho {
+
+class Texture : public Resource {
+public:
+    uint32_t getWidth() const { return width_; }
+    uint32_t getHeight() const { return height_; }
+    virtual const void* getNativeHandle() const = 0;
+
+protected:
+    uint32_t width_;
+    uint32_t height_;
+};
+
+}
+
+#endif
--- framework/rendering/backend/vulkan/VulkanTexture.hpp
+++ framework/rendering/backend/vulkan/VulkanTexture.hpp
@@ -1,11 +1,16 @@
 #ifndef SUISHO_RENDERING_BACKEND_VULKAN_TEXTURE_H_
 #define SUISHO_RENDERING_BACKEND_VULKAN_TEXTURE_H_
 
+#include "rendering/Texture.hpp"
 #include "rendering/backend/vulkan/VulkanImage.hpp"
 
 namespace suisho::backend {
 
-struct VulkanTexture {
+struct VulkanTexture : public Texture {
+    const void* getNativeHandle() const override {
+        return this;
+    }
+
     VulkanImage image;
     VkSampler sampler;
 };
